009664684276b1a3b7f07257a89c68b7ba473f93
Shashank Jarmale <shashankjarmale1@gmail.com>
2024-12-02 00:16:47 -0500

    Day 2 2024

---
+++ 2024/day2/p2-solution.py
@@ -0,0 +1,48 @@
+def load_input():
+    file = open('input.txt', 'r')
+    lines = file.read().splitlines()
+    return lines
+
+
+def safe_report(report):
+    if report[1] > report[0]:
+        increasing = True
+    else:
+        increasing = False
+
+    for i in range(1, len(report)):
+        if increasing and report[i] - report[i - 1] >= 1 and report[i] - report[i - 1] <= 3:
+            continue
+        elif not increasing and report[i - 1] - report[i] >= 1 and report[i - 1] - report[i] <= 3:
+            continue
+        else:
+            return False
+        
+    return True
+
+
+
+def main():
+    lines = load_input()
+    reports = []
+    for i, line in enumerate(lines):
+        nums = line.split(" ")
+        for i in range(len(nums)):
+            nums[i] = int(nums[i])
+        reports.append(nums)
+    
+    res = 0
+    for report in reports:
+        if safe_report(report):
+            res += 1
+        else:
+            for i in range(len(report)):
+                new_report = report[:i] + report[i+1:]
+                if safe_report(new_report):
+                    res += 1
+                    break
+
+    print(res)
+
+
+main()
