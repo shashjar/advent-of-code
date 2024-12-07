e2c749eec040972ac5d71330787bdfe8107df2c7
Shashank Jarmale <shashankjarmale1@gmail.com>
2024-12-07 11:09:28 -0500

    2024 Day 7

---
+++ 2024/day7/p2-solution.py
@@ -0,0 +1,30 @@
+def load_input():
+    file = open('input.txt', 'r')
+    lines = file.read().splitlines()
+    return lines
+
+
+def equation_possibly_true(nums, test_value):
+    if len(nums) == 1:
+        return nums[0] == test_value
+    
+    add = equation_possibly_true([nums[0] + nums[1]] + nums[2:], test_value)
+    mul = equation_possibly_true([nums[0] * nums[1]] + nums[2:], test_value)
+    concat = equation_possibly_true([int(str(nums[0]) + str(nums[1]))] + nums[2:], test_value)
+    return add or mul or concat
+
+
+def main():
+    lines = load_input()
+    res = 0
+    for i, line in enumerate(lines):
+        parts = line.split(":")
+        test_value = int(parts[0])
+        nums = list(map(lambda x: int(x), parts[1].split(" ")[1:]))
+        if equation_possibly_true(nums, test_value):
+            res += test_value
+        
+    print(res)
+
+
+main()
