c8470d5bce9b8b34b2f8c99a6f3c7c66668a8be6
Shashank Jarmale <shashankjarmale1@gmail.com>
2024-12-09 19:21:29 -0500

    2024 Day 8

---
+++ 2024/day8/p1-solution.py
@@ -0,0 +1,71 @@
+from collections import defaultdict
+
+
+def load_input():
+    file = open('input.txt', 'r')
+    lines = file.read().splitlines()
+    return lines
+
+
+def on_grid(pos, matrix):
+    return pos[0] >= 0 and pos[0] < len(matrix) and pos[1] >= 0 and pos[1] < len(matrix[0])
+
+
+def calculate_antinode_positions(matrix, pos1, pos2):
+    antinode_positions = []
+
+    row_diff, col_diff = pos2[0] - pos1[0], pos2[1] - pos1[1]
+    antinode_pos1 = (pos2[0] + row_diff, pos2[1] + col_diff)
+    antinode_pos2 = (pos1[0] - row_diff, pos1[1] - col_diff)
+
+    if on_grid(antinode_pos1, matrix):
+        antinode_positions.append(antinode_pos1)
+
+    if on_grid(antinode_pos2, matrix):
+        antinode_positions.append(antinode_pos2)
+
+    return antinode_positions
+
+
+def calculate_frequency_unique_antinode_locations(matrix, positions):
+    unique_antinode_locations = set()
+
+    for i in range(len(positions)):
+        for j in range(i + 1, len(positions)):
+            pos1, pos2 = positions[i], positions[j]
+            antinode_positions = calculate_antinode_positions(matrix, pos1, pos2)
+            unique_antinode_locations.update(antinode_positions)
+        
+    return unique_antinode_locations
+
+
+def num_unique_antinode_locations(matrix, antenna_positions):
+    unique_antinode_locations = set()
+
+    for frequency in antenna_positions:
+        frequency_unique_antinode_locations = calculate_frequency_unique_antinode_locations(matrix, antenna_positions[frequency])
+        unique_antinode_locations.update(frequency_unique_antinode_locations)
+
+    return len(unique_antinode_locations)
+
+
+def main():
+    matrix = []
+
+    antenna_positions = defaultdict(list)
+
+    lines = load_input()
+    for r, line in enumerate(lines):
+        row = []
+        for c, char in enumerate(line):
+            row.append(char)
+
+            if char != ".":
+                antenna_positions[char].append((r, c))
+        matrix.append(row)
+    
+    res = num_unique_antinode_locations(matrix, antenna_positions)
+    print(res)
+
+
+main()
